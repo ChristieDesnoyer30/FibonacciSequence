a2a99c7034276f726b81030fdb96f9f55f12ba13
Christie Desnoyer <christiedesnoyer@Christies-MacBook-Pro.local>
2019-01-18 14:55:49 -0500

    Initializing variables

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Each new number in the Fibonacci sequence is calculated by adding the previous two numbers.
+        // By considering the terms in the Fibonacci sequence whose values do not exceed 4 million,
+        // write a program to find the sum of the even-valued terms.
+
+        int firstNum = 0;
+        int secondNum = 1;
+
+
+
+        
+
+
+
+
+
+    }
+}
